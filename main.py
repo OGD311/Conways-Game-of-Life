228b5ef47c6ea7fe1b9319db73806189a6edae85
OGD <114223604+OGD311@users.noreply.github.com>
2025-06-01 00:46:03 +0100

    Rewrote the way the state is handled to make use of two grid arrays. These hold the current state and the next state, so that all calculations can be undertook before the next state is drawn. Added ability to pause/unpause so that the user can draw a shape.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import sys
 WIDTH, HEIGHT = 600, 600
 CELL_SIZE = 10
 
+ROWS = WIDTH // CELL_SIZE
+COLUMNS = HEIGHT // CELL_SIZE
+
 ALIVE = (255, 255, 255)
 DEAD = (0, 0, 0)
 
@@ -13,63 +16,85 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+grid = [[DEAD for _ in range(COLUMNS)] for _ in range(ROWS)]
 
 
+def draw_grid():
+    for y in range(ROWS):
+        for x in range(COLUMNS):
+            pygame.draw.rect(screen, grid[y][x], (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+
+def count_neighbours(x, y):
+    neighbourCount = 0
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if (0 <= i < COLUMNS and 0 <= j < ROWS):
+                if not (i == x and j == y):
+                    neighbourState = grid[j][i]
+
+                    if neighbourState == ALIVE:
+                        neighbourCount += 1
+
+
+    return neighbourCount
+
+paused = True
 running = True
 while running:
 
     for event in pygame.event.get():
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                paused = not paused
+        
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            x = (x // CELL_SIZE) * CELL_SIZE
-            y = (y // CELL_SIZE) * CELL_SIZE
+            x = (x // CELL_SIZE)
+            y = (y // CELL_SIZE)
 
-            state = screen.get_at((x, y))
-
-            if state == ALIVE:
-                pygame.draw.rect(screen, DEAD, (x, y, CELL_SIZE, CELL_SIZE), 0)
-            else:
-                pygame.draw.rect(screen, ALIVE, (x, y, CELL_SIZE, CELL_SIZE), 0)
+            grid[y][x] = DEAD if grid[y][x] == ALIVE else ALIVE
 
 
 
-    for x in range(0, WIDTH, CELL_SIZE):
-        for y in range(0, HEIGHT, CELL_SIZE):
-
-            state = screen.get_at((x, y))
-
-            # Get neighbour count - we do this regardless of state as there are both rules for 'Alive' and 'Dead' cells
-            neighbourCount = 0
-            for i in range(x - 1, x + 2):
-                for j in range(y - 1, y + 2):
-                    if (i >= 0 and j >= 0) and (i < WIDTH and j < HEIGHT) and (i != x and j != y):
-                        neighbourState = screen.get_at((i, j))
-
-                        if neighbourState == ALIVE:
-                            neighbourCount += 1
 
 
-            if state == ALIVE:
+    if not paused:
+        new_grid = [[DEAD for _ in range(COLUMNS)] for _ in range(ROWS)]
+        
+        
+        for x in range(COLUMNS):
+            for y in range(ROWS):
+                state = grid[y][x]
 
-                # Rule 1 - Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-                if neighbourCount < 2:
-                    pygame.draw.rect(screen, DEAD, (x, y, CELL_SIZE, CELL_SIZE), 0)
+                # Get neighbour count - we do this regardless of state as there are both rules for 'Alive' and 'Dead' cells
+                neighbourCount = count_neighbours(x, y)
 
-                # Rule 2 - Any live cell with two or three live neighbours lives on to the next generation.
-                # -- No need to do anything for this - the cell stays 'Alive' if we do not edit it
+                if state == ALIVE:
 
-                # Rule 3 - Any live cell with more than three live neighbours dies, as if by overpopulation.
-                if neighbourCount > 3:
-                    pygame.draw.rect(screen, DEAD, (x, y, CELL_SIZE, CELL_SIZE), 0)
-            
+                    # Rule 1 - Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                    if neighbourCount < 2:
+                        new_grid[y][x] = DEAD
 
-            if state == DEAD and neighbourCount == 3:
+                    # Rule 3 - Any live cell with more than three live neighbours dies, as if by overpopulation.
+                    elif neighbourCount > 3:
+                        new_grid[y][x] = DEAD
 
-                pygame.draw.rect(screen, ALIVE, (x, y, CELL_SIZE, CELL_SIZE), 0)
+                    # Rule 2 - Any live cell with two or three live neighbours lives on to the next generation.
+                    else:
+                        new_grid[y][x] = ALIVE
 
+                if state == DEAD and neighbourCount == 3:
+
+                    new_grid[y][x] = ALIVE
+
+        grid = new_grid
+    
+    draw_grid()
     pygame.display.flip()
     clock.tick(FPS)
 
